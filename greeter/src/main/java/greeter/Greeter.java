fba2bf799306b9561b81139984057ca85ec78904
urugang <urugang@gmail.com>
2018-08-20 14:54:36 +0800

    add javaexec

--- greeter/src/main/java/greeter/Greeter.java
+++ greeter/src/main/java/greeter/Greeter.java
@@ -3,6 +3,8 @@ package greeter;
 public class Greeter {
     public static void main(String[] args) {
         final String output = GreetingFormatter.greeting(args[0]);
-        System.out.println(output);
+        System.out.printf("from command line: %s\n", output);
+
+        // System.out.printf("from command stdin: %s\n", System.in.read());
     }
 }
