30436aa210a1137293e3919e814c897c9377c39a
urugang <urugang@gmail.com>
2018-08-21 11:54:47 +0800

    update mirror

--- greeter/src/main/java/greeter/Greeter.java
+++ greeter/src/main/java/greeter/Greeter.java
@@ -3,7 +3,7 @@ package greeter;
 public class Greeter {
     public static void main(String[] args) {
         final String output = GreetingFormatter.greeting(args[0]);
-        System.out.printf("from command line: %s\n", output);
+        System.out.printf("%s\n", output);
 
         // System.out.printf("from command stdin: %s\n", System.in.read());
     }
